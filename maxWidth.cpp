2c3e5def9e3567e456bcbb69c51715438f40ab49
Deepak Sharma <deepsrboy&gmail.com>
2022-06-20 09:05:52 +0530

    day17 challenge110

---
+++ maxWidth.cpp
@@ -0,0 +1,49 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+
+
+int getMaxWidth(TreeNode<int> *root)
+{
+    if(root == NULL)
+        return 0;
+    int ans = 1;
+    queue<TreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        int sz = q.size();
+        ans = max(ans,sz);
+        for(int i=0;i<sz;i++)
+        {
+            TreeNode<int> *node = q.front();
+            q.pop();
+            if(node->left)
+            {
+                q.push(node->left);
+            }
+            if(node->right)
+            {
+                q.push(node->right);
+            }
+        }
+    }
+    return ans;
+}
