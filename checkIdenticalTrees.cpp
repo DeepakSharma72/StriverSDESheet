c6ee50d5d6f62fe9ab9b1a0220dbd7548c45bd7f
Deepak Sharma <deepsrboy&gmail.com>
2022-06-21 08:18:16 +0530

    day18 challenge114

---
+++ checkIdenticalTrees.cpp
@@ -0,0 +1,30 @@
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+
+    class BinaryTreeNode {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+		
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+***********************************************************/
+
+bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
+   	if(root1 == NULL && root2 == NULL)
+        return true;
+    if(root1 == NULL || root2 == NULL)
+        return false;
+    if(root1->data != root2->data)
+        return false;
+    return (identicalTrees(root1->left,root2->left) & identicalTrees(root1->right,root2->right));
+}
