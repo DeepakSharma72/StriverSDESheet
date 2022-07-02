57365629159c158b7ae4ba92b344e76f9f29f80b
Deepak Sharma <deepsrboy&gmail.com>
2022-07-02 09:53:15 +0530

    day27 challenge173

---
+++ minPathSum.cpp
@@ -0,0 +1,18 @@
+int R = grid.size();
+        int C = grid[0].size();
+        for(int i=1;i<R;i++)
+        {
+            grid[i][0] += grid[i-1][0];
+        }
+        for(int j=1;j<C;j++)
+        {
+            grid[0][j] += grid[0][j-1];
+        }
+        for(int i=1;i<R;i++)
+        {
+            for(int j=1;j<C;j++)
+            {
+                grid[i][j] = grid[i][j] + min(grid[i-1][j],grid[i][j-1]);
+            }
+        }
+        return grid[R-1][C-1];
